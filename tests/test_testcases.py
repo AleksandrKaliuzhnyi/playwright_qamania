fa4319cf4945a66091062dada8d8b20b160eed70
Oleksandr <aleksandr.kaliuzhnyi@gmail.com>
2021-11-30 19:09:04 +0200

    Data-driven testing

--- tests/test_testcases.py
+++ tests/test_testcases.py
@@ -1,25 +1,23 @@
-def test_new_testcase(desktop_app_auth):
-    test_name = 'hello'
+from pytest import mark
+
+
+"""data = [('hello', 'world'),
+        ('hello', ''),
+        ('123', 'world')]"""
+
+ddt = {
+    'argnames': 'name, description',
+    'argvalues': [
+        ('hello', 'world'),
+        ('hello', ''),
+        ('123', 'world')],
+    'ids': ['general test', 'test no description', 'test with digits in name']
+}
+# @mark.parametrize(argnames='name, description', argvalues=data, ids=['general test', 'test no description', 'test with digits in name'])
+@mark.parametrize(**ddt)
+def test_new_testcase(desktop_app_auth, name, description):
     desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, 'world')
+    desktop_app_auth.create_test(name, description)
     desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
-
-
-def test_new_testcase_no_descr(desktop_app_auth):
-    test_name = 'hello'
-    desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, '')
-    desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
-
-
-def test_new_testcase_digits_name(desktop_app_auth):
-    test_name = '123'
-    desktop_app_auth.navigate_to('Create new test')
-    desktop_app_auth.create_test(test_name, '')
-    desktop_app_auth.navigate_to('Test Cases')
-    assert desktop_app_auth.test_cases.check_test_exists(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
+    assert desktop_app_auth.test_cases.check_test_exists(name)
+    desktop_app_auth.test_cases.delete_test_by_name(name)
