7f1f14a1e68c53305514b0d74f9361b3bf8d1763
Oleksandr <aleksandr.kaliuzhnyi@gmail.com>
2021-12-02 16:56:25 +0200

    added webkit and firefox browsers to params, added mobile devices, new fixture created

--- conftest.py
+++ conftest.py
@@ -13,8 +13,27 @@ def get_playwright():
         yield playwright
 
 
+@fixture(scope='session', params=['chromium', 'firefox', 'webkit'], ids=['chromium', 'firefox', 'webkit'])
+def get_browser(get_playwright, request):
+    browser = request.param
+    headless = request.config.getini('headless')
+    if headless == 'True':
+        headless = True
+    else:
+        headless = False
+
+    if browser == 'chromium':
+        yield get_playwright.chromium.launch(headless=headless)
+    elif browser == 'firefox':
+        yield get_playwright.firefox.launch(headless=headless)
+    elif browser == 'webkit':
+        yield get_playwright.webkit.launch(headless=headless)
+    else:
+        assert False, 'browser type unknown'
+
+
 @fixture(scope='session')
-def desktop_app(get_playwright, request):
+def desktop_app(get_browser, request):
     # conditions if use addoption
     # base_url = request.config.getoption('--base_url')
     # conditions if use getini
@@ -22,7 +41,7 @@ def desktop_app(get_playwright, request):
     # conditions if use addoption
     # conditions if use json config
     base_url = request.config.getoption('--base_url')
-    app = App(get_playwright, base_url=base_url, **BROWSER_OPTIONS)
+    app = App(get_browser, base_url=base_url, **BROWSER_OPTIONS)
     # conditions if use settings.py
     # app = App(get_playwright,  base_url=settings.BASE_URL)
     app.goto('/')
@@ -39,8 +58,8 @@ def desktop_app_auth(desktop_app, request):
     yield desktop_app
 
 
-@fixture(scope='session')
-def mobile_app(get_playwright, request):
+@fixture(scope='session', params=['iPhone 11', 'Pixel 2'], ids=['iPhone 11', 'Pixel 2'])
+def mobile_app(get_playwright, get_browser, request):
     # conditions if use addoption
     # base_url = request.config.getoption('--base_url')
     # conditions if use getini
@@ -48,8 +67,13 @@ def mobile_app(get_playwright, request):
     # conditions if use addoption
     # conditions if use json config
     base_url = request.config.getoption('--base_url')
-    device = request.config.getoption('--device')
-    app = App(get_playwright, base_url=base_url, device=device, **BROWSER_OPTIONS)
+    device = request.config.param
+    device_config = get_playwright.devices.get(device)
+    if device_config is not None:
+        device_config.update(BROWSER_OPTIONS)
+    else:
+        device_config = BROWSER_OPTIONS
+    app = App(get_browser, base_url=base_url, **device_config)
     # conditions if use settings.py
     # app = App(get_playwright,  base_url=settings.BASE_URL)
     app.goto('/')
@@ -74,6 +98,8 @@ def pytest_addoption(parser):
     # json config
     parser.addoption('--secure', action='store', default='secure.json')
     parser.addoption('--device', action='store', default='')
+    parser.addoption('--browser', action='store', default='chromium')
+    parser.addini('headless', help='run tests in headless mode', default='False')
 
 
 def load_config(file):
--- page_objects/application.py
+++ page_objects/application.py
@@ -1,16 +1,11 @@
-from playwright.sync_api import Playwright
+from playwright.sync_api import Browser
 from page_objects.test_cases import TestCases
 
 
 class App:
-    def __init__(self, playwright: Playwright, base_url: str, headless=False, device=None, **kwargs):
-        device_config = playwright.devices.get(device)
-        if device_config is not None:
-            device_config.update(kwargs)
-        else:
-            device_config = kwargs
-        self.browser = playwright.chromium.launch(headless=headless)
-        self.context = self.browser.new_context(**device_config)
+    def __init__(self, browser: Browser, base_url: str, **kwargs):
+        self.browser = browser
+        self.context = self.browser.new_context(**kwargs)
         self.page = self.context.new_page()
         self.base_url = base_url
         self.test_cases = TestCases(self.page)
@@ -23,6 +18,7 @@ class App:
 
     def navigate_to(self, menu: str):
         self.page.click(f"css=header >> text={menu}")
+        self.page.wait_for_load_state()
 
     def login(self, login: str, password: str):
         self.page.fill("input[name=\"username\"]", login)
