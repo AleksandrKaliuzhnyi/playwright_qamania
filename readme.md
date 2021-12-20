d428f888fdf1da119c3a74d8eef90a9f35faa0fd
Oleksandr <aleksandr.kaliuzhnyi@gmail.com>
2021-12-20 20:56:17 +0200

    added test parameters

--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ about...
 
 ## Allure report
 allure serve .\report\ --port 3060
+
+
+## Pytest
+pytest -m smoke
+pytest -m smoke --maxfail=1
+pytest -n smoke -n 2 - run tests in several treads
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,6 +1,8 @@
 import allure
+from pytest import mark
 
 
+@mark.smoke
 @allure.title('test_wait_more_30_sec')
 def test_wait_more_30_sec(desktop_app_auth):
     desktop_app_auth.navigate_to('Demo pages')
@@ -8,6 +10,7 @@ def test_wait_more_30_sec(desktop_app_auth):
     assert desktop_app_auth.demo_pages.check_wait_page()
 
 
+@mark.smoke
 @allure.title('test_ajax')
 def test_ajax(desktop_app_auth):
     desktop_app_auth.navigate_to('Demo pages')
@@ -15,6 +18,7 @@ def test_ajax(desktop_app_auth):
     assert 2 == desktop_app_auth.demo_pages.get_ajax_responses_count()
 
 
+@mark.smoke
 @allure.title('test_handlers')
 def test_handlers(desktop_app_auth):
     desktop_app_auth.navigate_to('Demo pages')
